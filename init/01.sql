7591c390a374218c14c70d321407501405094799
NicolasV <nivalderramas@unal.edu.co>
2022-09-11 19:48:32 -0500

    Typo xd

--- init/01.sql
+++ init/01.sql
@@ -1,14 +1,14 @@
 CREATE TABLE auth (
-	id varchar(20) NOT NULL,
-	username varchar(20) NOT NULL,
-	password varchar(20) NOT NULL,
+	id varchar(100) NOT NULL,
+	username varchar(100) NOT NULL,
+	password varchar(100) NOT NULL,
 	PRIMARY KEY(id)
 );
 CREATE TABLE user (
-	id varchar(20) NOT NULL,
-	username varchar(20) NOT NULL,
-	name varchar(20) NOT NULL,
-	rol varchar(20) NOT NULL DEFAULT 'student',
+	id varchar(100) NOT NULL,
+	username varchar(100) NOT NULL,
+	name varchar(100) NOT NULL,
+	rol varchar(100) NOT NULL DEFAULT 'student',
 	PRIMARY KEY (id),
 	FOREIGN KEY (id) REFERENCES auth(id)
 );
