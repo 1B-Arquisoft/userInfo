851f27753dce885882ed9f0f4f97e8a9319427d5
NicolasV <nivalderramas@unal.edu.co>
2022-09-12 09:51:32 -0500

    Add missing fields in schema

--- init/01.sql
+++ init/01.sql
@@ -9,6 +9,11 @@ CREATE TABLE user (
 	username varchar(100) NOT NULL,
 	name varchar(100) NOT NULL,
 	rol varchar(100) NOT NULL DEFAULT 'student',
+	email varchar(100) NOT NULL DEFAULT 'new_student@gmail.com',
+	institution_email varchar(100) NOT NULL DEFAULT '@unal.edu.co',
+	address varchar(100) NOT NULL DEFAULT '@unal.edu.co',
+	cellphone_number varchar(100),
+	born_date date,
 	PRIMARY KEY (id),
 	FOREIGN KEY (id) REFERENCES auth(id)
 );
