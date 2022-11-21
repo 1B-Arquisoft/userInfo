eaba56b24840a1504b9b15cabae204bdeae4eb9b
NicolasV <nivalderramas@unal.edu.co>
2022-11-20 20:54:50 -0500

    change db connection

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ module.exports = {
   },
   mysql: {
     database: process.env.MYSQL_DB || 'USER_INFORMATION',
-    user: process.env.MYSQL_USER || 'grupo1b',
-    password: process.env.MYSQL_PASSWORD || 'cristianoronaldo',
-    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || 'contrasenasegura',
+    host: process.env.MYSQL_HOST || '34.170.38.178',
     port: process.env.MYSQL_PORT || 3306,
   }
 }
